eb0b69cb31ae5e53b76ae4a309abaa00946a01d0
yaraaos <yara144ss@gmail.com>
2024-10-25 00:34:48 +0300

    done

--- js/task-1.js
+++ js/task-1.js
@@ -1,28 +1,30 @@
-function isEnoughCapacity(products, containerSize) {
-    let totalProducts = 0;
+function isEnoughCapacity(products, containerSize){
 
-    for (const product in products) {
-      totalProducts += products[product];
-    }
+  let total = 0;
 
-    return totalProducts <= containerSize;
+  const values = Object.values(products);
+
+  for (const value of values){
+    total += value;
   }
   
+  return total <= containerSize;
+};
 
 
-  console.log(
-    isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
-  ); // true
-  
-  console.log(
-    isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
-  ); // false
-  
-  console.log(
-    isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
-  ); // true
-  
-  console.log(
-    isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
-  ); // false
-  
+
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); 
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); 
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); 
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+);
